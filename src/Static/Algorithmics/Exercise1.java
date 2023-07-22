06a356de53889087eed944c12b4943b2d46d0ba3
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-07-22 18:30:53 +0200

    Algorithmics - exercise 1

---
+++ src/Static/Algorithmics/Exercise1.java
@@ -0,0 +1,20 @@
+package Static.Algorithmics;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int input = 0;
+
+        do {
+            sum += input;
+            System.out.println("Insert number: ");
+            input = scanner.nextInt();
+        } while (input <= 100);
+
+        if (sum % 2 == 0) System.out.println("Sum is even." + sum);
+        else System.out.println("Sum is odd." + sum);
+    }
+}
