9e1d8a06e0d5715ea13700363365c7ead9d87e81
Bartosz Cłapiński <bartosz.clapinski@gmail.com>
2023-07-25 14:53:43 +0200

    Super - Exercise

---
+++ src/Super/Exercise1/GroceryShop.java
@@ -0,0 +1,9 @@
+package Super.Exercise1;
+
+public class GroceryShop {
+    public static void main(String[] args) {
+        Apple apple = new Apple(100, "Golden", "Golden Delicious");
+
+        System.out.println(apple.getInfo());
+    }
+}
