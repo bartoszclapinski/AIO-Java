985fca2fc6292e510c4e5b8d36f868c626ccf057
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-08-12 18:29:35 +0200

    Object - hashCode

--- src/Object/School.java
+++ src/Object/School.java
@@ -4,8 +4,14 @@ public class School {
     public static void main(String[] args) {
         Student student1 = new Student("John", 20);
         Student student2 = new Student("John", 20);
+        Student student3 = new Student("Jane", 20);
 
         System.out.println(student1.equals(student2));
         System.out.println(student1 == student2);
+
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+        System.out.println(student3.hashCode());
+
     }
 }
