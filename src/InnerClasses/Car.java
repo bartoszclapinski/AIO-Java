f3bbcf777a96192b3050bfc96ff37d8b4713fab0
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-10-05 19:06:36 +0200

    Inner classes

---
+++ src/InnerClasses/Car.java
@@ -0,0 +1,61 @@
+package InnerClasses;
+
+public class Car {
+    private final Engine engine;
+    private static int fuel;
+
+    public Car() {
+        engine = new Engine("Porsche 3.8 Flat-6");
+        System.out.println("Car with " + engine.engineType + " engine created.");
+    }
+
+    public void refuel(int liters){
+        fuel += liters;
+    }
+
+    public void go() throws InterruptedException {
+        while (fuel > 0) {
+            engine.consumeFuel();
+            System.out.println("Car is moving.");
+            Thread.sleep(1000);
+        }
+        System.out.println("No fuel!");
+    }
+
+    public class Engine {
+        private String engineType;
+        private static final int FUEL_CONSUMPTION = 20;
+
+        public Engine(String engineType) {
+            this.engineType = engineType;
+        }
+
+        public void consumeFuel() {
+            if (fuel >= FUEL_CONSUMPTION) {
+                fuel = fuel - FUEL_CONSUMPTION;
+            } else {
+                fuel = 0; // same as Car.this.fuel = 0;
+                System.out.println("No fuel!");
+            }
+        }
+    }
+
+    public static class StaticEngine {
+        private String engineType;
+        private static final int FUEL_CONSUMPTION = 20;
+
+        public StaticEngine(String engineType) {
+            this.engineType = engineType;
+        }
+
+        // fuel field needs to be static
+        public void consumeFuel() {
+            if (fuel >= FUEL_CONSUMPTION) {
+                fuel = fuel - FUEL_CONSUMPTION;
+            } else {
+                fuel = 0; // same as Car.this.fuel = 0;
+                System.out.println("No fuel!");
+            }
+        }
+    }
+}
---
+++ src/InnerClasses/MainApp.java
@@ -0,0 +1,12 @@
+package InnerClasses;
+
+public class MainApp {
+    public static void main(String[] args) throws InterruptedException {
+        Car car = new Car();
+        car.refuel(80);
+        car.go();
+
+        Car.Engine engine = car.new Engine("Porsche 3.8 Flat-6");
+        Car.StaticEngine staticEngine = new Car.StaticEngine("Porsche 3.8 Flat-6");
+    }
+}
